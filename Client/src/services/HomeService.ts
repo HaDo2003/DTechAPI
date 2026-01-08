e79acfa9eed112c0f57ab6ca06c2b43438cf6aa2
Ha Do <118713300+HaDo2003@users.noreply.github.com>
2026-01-08 23:33:54 +0700

    Change url to instance

--- Client/src/services/HomeService.ts
+++ Client/src/services/HomeService.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+// import axios from "axios";
+import axios from "../utils/axiosInstance";
 import type { Advertisement } from "../types/Advertisment";
 import type { Product } from "../types/Product";
 
